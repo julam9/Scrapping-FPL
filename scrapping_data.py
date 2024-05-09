c8bde89a3a253f3f23074554bc8b08fa9bc09761
Julio Antonio <julioam766@gmail.com>
2024-05-10 00:33:37 +0700

    modified : scrapping_data.py

--- scrapping_data.py
+++ scrapping_data.py
@@ -4,4 +4,10 @@ import pandas as pd
 
 base_url = "https://fantasy.premierleague.com/api/"
 r = requests.get(base_url+"bootstrap-static/").json()
-pprint(r, indent=2, depth=5, compact=False)
+#pprint(r, indent=3, depth=3, compact=False)
+
+# take the player detail info 
+players = pd.json_normalize(r["elements"])
+
+# make player df into csv 
+players.to_csv("player_data.csv", index=False)
